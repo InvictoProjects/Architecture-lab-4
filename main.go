3d68684b19d050e06e9b3e1e1b79b33b0043e850
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-12-12 17:31:07 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"os"
+	"strings"
+
+	"github.com/InvictoProjects/Architecture-lab-4/engine"
+	"github.com/InvictoProjects/Architecture-lab-4/tools"
+)
+
+const inputFile = "./test/commands.txt"
 
 func main() {
-	fmt.Println("Hello world!")
+	eventLoop := new(engine.EventLoop)
+	eventLoop.Start()
+	if input, err := os.Open(inputFile); err == nil {
+		defer input.Close()
+		scanner := bufio.NewScanner(input)
+		for scanner.Scan() {
+			commandLine := strings.TrimSpace(scanner.Text())
+			if commandLine == "" {
+				continue
+			}
+			cmd := tools.Parse(commandLine)
+			eventLoop.Post(cmd)
+		}
+	}
+	eventLoop.AwaitFinish()
 }
